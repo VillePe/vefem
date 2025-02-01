f3fd28179c6e49280e15f5eeff10e191e45d3061
Ville Pekkala <ville.pekkala@hotmail.com>
2025-02-01 22:25:47 +0200

    Initially implement load struct

--- src/loads/load.rs
+++ src/loads/load.rs
@@ -1,8 +1,94 @@
+use crate::loads::load::LoadType::{Line, Moment, Point, Strain, Temperature, Trapezoid, Triangular};
+
 pub struct Load {
+    /// Name for the load. Does not need to be unique. Load combinations are created by using the load names.
     pub name : String,
-    pub offset_start : f64,
-    pub offset_end : f64,
+    /// The numbers for which this load is applied to. Different elements are separated by , (comma)
+    /// Can be used with <S..E> (double dots with numbers before and after it) to imply that elements 
+    /// from 'S' to 'E' are included (the element with number 'E' is also included) 
+    pub element_numbers: String,
+    /// The load type for the load. Controls how the load needs to be handled.
+    pub load_type : LoadType,
+    /// The offset for the load measured from the start of the element to the start of the load.
+    /// Always in the X-axis of the elements local coordinates. 
+    pub offset_start : String,
+    /// The offset for the load measured from the start of the element to the end of the load.
+    /// Always in the X-axis of the elements local coordinates.
+    pub offset_end : String,
+    /// Comment for the load 
     pub comment: String,
+    /// The strength of the load (can be an equation). For trapezoid loads, the start and end values are separated
+    /// with ; (semicolon)
+    pub strength : String,
+    /// The rotation of the load. 0 means the load is pointing towards positive X-axis in global coordinates (right).
     pub rotation: f64,
+    /// Controls whether the load should be moved between elements in different load combinations.
+    /// Only matters in load combinations that are automatically created
+    pub is_moving_load: bool,
+    /// The percentage of the value that is set on the 'off element' when creating the load combinations with
+    /// moving loads. Default value is 0 ('off element' has no load)
+    pub moving_percent: f64,
+}
+
+impl Load {
+    pub fn new() -> Self {
+        Self {
+            ..Self::default()
+        }
+    }
     
+    pub fn new_point_load(name: String, element_numbers: String, offset_start: String, strength: String, rotation: f64) -> Self {
+        Self{name, element_numbers, offset_start, strength, rotation, load_type: Point, ..Self::default()}
+    }
+
+    pub fn new_line_load(name: String, element_numbers: String, offset_start: String, offset_end: String, strength: String, rotation: f64) -> Self {
+        Self{name, element_numbers, offset_start, offset_end, strength, rotation, load_type: Line, ..Self::default()}
+    }
+
+    pub fn new_moment_load(name: String, element_numbers: String, offset_start: String, strength: String, rotation: f64) -> Self {
+        Self{name, element_numbers, offset_start, strength, rotation, load_type: Moment, ..Self::default()}
+    }
+
+    pub fn new_triangular_load(name: String, element_numbers: String, offset_start: String, offset_end: String, strength: String, rotation: f64) -> Self {
+        Self{name, element_numbers, offset_start, offset_end, strength, rotation, load_type: Triangular, ..Self::default()}
+    }
+
+    pub fn new_trapezoid_load(name: String, element_numbers: String, offset_start: String, offset_end: String, strength: String, rotation: f64) -> Self {
+        Self{name, element_numbers, offset_start, offset_end, strength, rotation, load_type: Trapezoid, ..Self::default()}
+    }
+
+    pub fn new_strain_load(name: String, element_numbers: String, strength: String, rotation: f64) -> Self {
+        Self{name, element_numbers, strength, rotation, load_type: Strain, ..Self::default()}
+    }
+
+    pub fn new_temperature_load(name: String, element_numbers: String, strength: String, rotation: f64) -> Self {
+        Self{name, element_numbers, strength, rotation, load_type: Temperature, ..Self::default()}
+    }
+}
+
+impl Default for Load {
+    fn default() -> Self {
+        Self {
+            name: "".to_string(),
+            load_type: LoadType::Point,
+            element_numbers: "".to_string(),
+            offset_start : "0".to_string(),
+            offset_end: "L".to_string(),
+            strength: "0".to_string(),
+            rotation: -90.0,
+            comment: "".to_string(),
+            is_moving_load: false,
+            moving_percent: 0.0,
+        }
+    }
+}
+
+pub enum LoadType {
+    Point,
+    Line,
+    Triangular,
+    Moment,
+    Trapezoid,
+    Strain,
+    Temperature,
 }
