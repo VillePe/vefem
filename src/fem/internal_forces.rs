40adeec511d0ccd6a8ea59d712492f1449423404
Ville Pekkala <ville.pekkala@hotmail.com>
2025-02-20 22:13:38 +0200

    Implement axial force calcs and tests

--- src/fem/internal_forces.rs
+++ src/fem/internal_forces.rs
@@ -80,9 +80,9 @@ pub fn calculate_shear_at(x: f64, element: &Element, nodes: &HashMap<i32, Node>,
             load::CalculationLoadType::Triangular => {
                 // Triangular load with max load at left hand side
                 if load.offset_start < load.offset_end {
-                    shear += shear_triang_ltr(load, x)
+                    shear += handle_linear_force_triang_ltr(load, x, z_dir_factor)
                 } else {
-                    shear += shear_triang_rtl(load, x)
+                    shear += handle_linear_force_triang_rtl(load, x, z_dir_factor)
                 }
                 
             }
@@ -93,6 +93,48 @@ pub fn calculate_shear_at(x: f64, element: &Element, nodes: &HashMap<i32, Node>,
     shear += local_reactions[(1, 0)];
 
     shear
+}
+
+pub fn calculate_axial_force_at(x: f64, element: &Element, nodes: &HashMap<i32, Node>, loads: &Vec<CalculationLoad>, results: &NodeResults) -> f64 {
+    let mut axial_f = 0.0;
+    let local_reactions = results.get_elem_local_reactions(element, nodes);
+    for load in loads {
+        // The factor to handle skewed loads
+        let x_dir_factor = -load.rotation.to_radians().cos();
+        match load.load_type {
+            load::CalculationLoadType::Point => {
+                if load.offset_start <= x {
+                    axial_f += load.strength * x_dir_factor;
+                }
+            }
+            load::CalculationLoadType::Rotational => {}
+            load::CalculationLoadType::Line => {
+                if load.offset_start <= x {
+                    let load_length;
+                    if load.offset_end <= x {
+                        load_length = load.offset_end - load.offset_start;
+                    } else {
+                        load_length = x - load.offset_start;                      
+                    }
+                    axial_f += load.strength * x_dir_factor * load_length;
+                }
+            }
+            load::CalculationLoadType::Triangular => {
+                // Triangular load with max load at left hand side
+                if load.offset_start < load.offset_end {
+                    axial_f += handle_linear_force_triang_ltr(load, x, x_dir_factor)
+                } else {
+                    axial_f += handle_linear_force_triang_rtl(load, x, x_dir_factor)
+                }
+                
+            }
+            load::CalculationLoadType::Strain => {}
+        };
+    }
+    
+    axial_f -= local_reactions[(0, 0)];
+
+    axial_f
 }
 
 /// Calculates the moment at x for a triangular load with the maximum load at the left hand side.
@@ -156,15 +198,16 @@ fn moment_triang_rtl(load: &CalculationLoad, x: f64) -> f64 {
     0.0
 }
 
-/// Calculates the moment at x for a triangular load with the maximum load at the left hand side.
+/// Calculates the shear or axial force at x for a triangular load with the maximum load at the left 
+/// hand side.
 /// ltr = Left to right
-fn shear_triang_ltr(load: &CalculationLoad, x: f64) -> f64 {
-    let mut shear = 0.0;
-    let z_dir_factor = load.rotation.to_radians().sin();
+/// * 'dir_factor' - the factor to handle the direction of the load. For shear use ``load.rotation.to_radians().sin()`` and for axial force use ``...cos()``)
+fn handle_linear_force_triang_ltr(load: &CalculationLoad, x: f64, dir_factor: f64) -> f64 {
+    let mut shear = 0.0;    
     if load.offset_start <= x {
         if load.offset_end <= x {
             let load_length = load.offset_end - load.offset_start;                          
-            shear += load.strength * z_dir_factor * load_length / 2.0;
+            shear += load.strength * dir_factor * load_length / 2.0;
         } else {
             // Split the load into a line load and a triangular load at x.
             let load_length = x - load.offset_start;         
@@ -177,17 +220,18 @@ fn shear_triang_ltr(load: &CalculationLoad, x: f64) -> f64 {
             let strength_ll = strength_min;
             let strength_tl = load.strength - strength_ll;
             // Moment from triangular load = F * l / 2 * offset
-            shear += strength_tl * z_dir_factor * load_length / 2.0;
-            shear += strength_ll * z_dir_factor * load_length;
+            shear += strength_tl * dir_factor * load_length / 2.0;
+            shear += strength_ll * dir_factor * load_length;
         }                        
     }
     shear
 }
 
-/// Calculates the moment at x for a triangular load with the maximum load at the right hand side.
+/// Calculates the shear or axial force at x for a triangular load with the maximum load at the 
+/// right hand side.
 /// ltr = Left to right
-fn shear_triang_rtl(load: &CalculationLoad, x: f64) -> f64 {
-    let z_dir_factor = load.rotation.to_radians().sin();
+/// * 'dir_factor' - the factor to handle the direction of the load. For shear use ``load.rotation.to_radians().sin()`` and for axial force use ``...cos()``)
+fn handle_linear_force_triang_rtl(load: &CalculationLoad, x: f64, dir_factor: f64) -> f64 {
     // Load offsets at left or right hand side
     let left = load.offset_end;
     let right = load.offset_start;
@@ -195,7 +239,7 @@ fn shear_triang_rtl(load: &CalculationLoad, x: f64) -> f64 {
         let load_length;
         if right <= x {
             load_length = right - left;                            
-            return load.strength * z_dir_factor * load_length / 2.0
+            return load.strength * dir_factor * load_length / 2.0
         } else {
             // Split the load at x. No need to split into a line load, because of 
             // the direction
@@ -205,7 +249,7 @@ fn shear_triang_rtl(load: &CalculationLoad, x: f64) -> f64 {
                 load.strength * 
                 (x - left) / 
                 (right - left);
-            return strength_max * z_dir_factor * load_length / 2.0
+            return strength_max * dir_factor * load_length / 2.0
         }                       
     }
     0.0
