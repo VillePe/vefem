c5853a2136b91fd1c845e5ae386a3430e073a779
Ville Pekkala <ville.pekkala@hotmail.com>
2025-03-02 18:47:54 +0200

    Move CalculationModel to its own file

--- src/structure/mod.rs
+++ src/structure/mod.rs
@@ -2,23 +2,11 @@
 mod node;
 mod release;
 mod support;
+mod calculation_model;
 pub mod utils;
-
-use std::collections::HashMap;
 
 pub use element::Element;
 pub use node::Node;
 pub use release::Release;
 pub use support::Support;
-
-use serde::{Deserialize, Serialize};
-use crate::{loads::{Load, LoadCombination}, settings::CalculationSettings};
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct CalculationModel {
-    pub nodes: HashMap<i32, Node>,
-    pub elements: Vec<Element>,
-    pub load_combinations: Vec<LoadCombination>,
-    pub loads: Vec<Load>,
-    pub calc_settings: CalculationSettings,
-}
+pub use calculation_model::CalculationModel;
