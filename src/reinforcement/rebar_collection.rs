96a2f2d1175b4316f804874d2576a72081800698
Ville Pekkala <ville.pekkala@hotmail.com>
2025-03-13 22:50:46 +0200

    Implement single rebars with polygon profile

--- src/reinforcement/rebar_collection.rs
+++ src/reinforcement/rebar_collection.rs
@@ -1,9 +1,14 @@
+#![allow(unused_assignments)]
+#![allow(unused_variables)]
 use std::f64::consts::PI;
 
 use serde::{Deserialize, Serialize};
-use vputilslib::{equation_handler::{self, EquationHandler}, geometry2d::Polygon};
+use vputilslib::{
+    equation_handler::EquationHandler,
+    geometry2d::polygon::Direction,
+};
 
-use crate::profile::{PolygonProfile, Profile};
+use crate::profile::Profile;
 
 use super::{
     reinforcement::{RebarDistribution, ReinforcementData},
@@ -27,20 +32,20 @@ pub struct RebarCollection {
     pub side: Side,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Copy, Clone, Serialize, Deserialize)]
 pub enum Side {
     /// The index argument controls where the rebar is placed.
     /// - 0 = bottom of the bounding box,
     /// - 1 = right,
     /// - 2 = top,
     /// - 3 = left
-    BoundingBox{index: i32},
+    BoundingBox { index: i32 },
     /// For polygon type, the index argument is by what line of the polygon the rebars are placed.
-    /// For this the orientation should be known and line count of the polygon needs to be known or 
+    /// For this the orientation should be known and line count of the polygon needs to be known or
     /// the program will panic if the index is out of the range of the lines.
-    Polygon{index: i32},
+    Polygon { index: i32 },
     /// For circular sections. The start angle is in degrees and starts from pointing right and is counter-clockwise.
-    Circular{start_angle: f64}
+    Circular { start_angle: f64 },
 }
 
 impl RebarCollection {
@@ -64,10 +69,18 @@ impl RebarCollection {
         }
     }
 
-    pub fn get_single_rebars(&self, profile: &Profile, equation_handler: &EquationHandler) -> Vec<CalculationRebar> {
-        let mut single_rebars: Vec<CalculationRebar> = Vec::new();        
-        let offset_start = equation_handler.calculate_formula(&self.offset_start).unwrap_or(0.0);
-        let offset_end = equation_handler.calculate_formula(&self.offset_end).unwrap_or(0.0);
+    pub fn get_single_rebars(
+        &self,
+        profile: &Profile,
+        equation_handler: &EquationHandler,
+    ) -> Vec<CalculationRebar> {
+        let mut single_rebars: Vec<CalculationRebar> = Vec::new();
+        let offset_start = equation_handler
+            .calculate_formula(&self.offset_start)
+            .unwrap_or(0.0);
+        let offset_end = equation_handler
+            .calculate_formula(&self.offset_end)
+            .unwrap_or(0.0);
         let mut x = 0.0;
         let mut y = 0.0;
         let row_length = match &self.side {
@@ -76,18 +89,14 @@ impl RebarCollection {
                 1 => profile.get_height(),
                 2 => profile.get_width(),
                 3 => profile.get_height(),
-                _ => panic!()
+                _ => panic!(),
             },
-            Side::Polygon { index } => {
-                match profile {
-                    Profile::PolygonProfile(polygon_profile) => {
-                        let (p1, p2) = polygon_profile.polygon.get_line_or_last(*index);                        
-                        vputilslib::geometry2d::calc_length_between_points(&p1, &p2)
-                    },
-                    _ => {
-                        profile.get_width()
-                    }
+            Side::Polygon { index } => match profile {
+                Profile::PolygonProfile(polygon_profile) => {
+                    let (p1, p2) = polygon_profile.polygon.get_line_or_last(*index);
+                    vputilslib::geometry2d::calc_length_between_points(&p1, &p2)
                 }
+                _ => profile.get_width(),
             },
             Side::Circular { start_angle } => todo!(),
         };
@@ -108,25 +117,27 @@ impl RebarCollection {
                     y = self.concrete_cover + diam / 2.0;
                     (x, y) = get_rebar_location_with_side(x, y, &self.side, profile);
                     single_rebars.push(CalculationRebar {
-                        area: PI * diam.powi(2)/4.0 , 
-                        x, 
+                        area: PI * diam.powi(2) / 4.0,
+                        x,
                         y,
-                        reinf_data: self.reinf_data.clone(), 
-                        offset_start: offset_start, 
-                        offset_end: offset_end});
+                        reinf_data: self.reinf_data.clone(),
+                        offset_start: offset_start,
+                        offset_end: offset_end,
+                    });
                 };
                 let spacing = (row_length - cc_right - cc_left - *diam) / (*count - 1) as f64;
-                for i in 0..*count { 
+                for i in 0..*count {
                     x = cc_left + diam / 2.0 + spacing * (i as f64);
                     y = self.concrete_cover + diam / 2.0;
                     (x, y) = get_rebar_location_with_side(x, y, &self.side, profile);
                     single_rebars.push(CalculationRebar {
-                        area: PI * diam.powi(2)/4.0 , 
-                        x, 
+                        area: PI * diam.powi(2) / 4.0,
+                        x,
                         y,
-                        reinf_data: self.reinf_data.clone(), 
-                        offset_start: offset_start, 
-                        offset_end: offset_end});
+                        reinf_data: self.reinf_data.clone(),
+                        offset_start: offset_start,
+                        offset_end: offset_end,
+                    });
                 }
             }
             RebarDistribution::Distributed { diam, distr } => {
@@ -149,8 +160,8 @@ impl RebarCollection {
 
 /// Gets the rebar location based on the side property of the rebar collection. See [RebarCollection::side]
 /// ### Arguments
-/// * `x` - The x position of the rebar from 0,0 before rotating
-/// * `y` - The y position of the rebar from 0,0 before rotating
+/// * `x` - The x position (center) of the rebar from 0,0 before translating or rotating
+/// * `y` - The y position (center) of the rebar from 0,0 before translating or rotating
 /// * `side` - The side of the rebar
 /// * `index` - The index of the rebar for Side::Polygon.
 /// * `profile` - The profile of the element
@@ -158,22 +169,43 @@ fn get_rebar_location_with_side(x: f64, y: f64, side: &Side, profile: &Profile) 
     let mid_w = profile.get_width() / 2.0;
     let mid_h = profile.get_height() / 2.0;
     match side {
-        Side::BoundingBox { index } => {
-            match index {
-                0 => (x, y),
-                1 => vputilslib::geometry2d::rotate(mid_w, mid_h, x, y, PI / 2.0),
-                2 => vputilslib::geometry2d::rotate(mid_w, mid_h, x, y, PI),
-                3 => vputilslib::geometry2d::rotate(mid_w, mid_h, x, y, 3.0*PI / 2.0),
-                _ => panic!(),
-            }
+        Side::BoundingBox { index } => match index {
+            0 => (x, y),
+            1 => vputilslib::geometry2d::rotate(mid_w, mid_h, x, y, PI / 2.0),
+            2 => vputilslib::geometry2d::rotate(mid_w, mid_h, x, y, PI),
+            3 => vputilslib::geometry2d::rotate(mid_w, mid_h, x, y, 3.0 * PI / 2.0),
+            _ => panic!(),
         },
-        Side::Polygon{index} => todo!(),
-        Side::Circular{start_angle} => todo!(),
+        Side::Polygon { index } => {
+            match profile {
+                Profile::PolygonProfile(polygon_profile) => {
+                    let polygon = &polygon_profile.polygon;
+                    let (p1, p2) = polygon.get_line_or_last(*index);
+                    let rotate_angle = vputilslib::geometry2d::get_angle_from_points(p1, p2);
+                    let mut yoffset = 0.0;
+                    // If the polygon is clockwise, the y value needs to be inversed
+                    if polygon.get_direction() == Direction::Clockwise { yoffset = -y * 2.0; }
+                    // Rotate the point around (0,0)
+                    let (mut rx, mut ry) =
+                        vputilslib::geometry2d::rotate(0.0, 0.0, x, y+yoffset, rotate_angle);
+                    // Move the rotated point with move vector from (0,0) to p1
+                    rx += p1.x;
+                    ry += p1.y;
+                    (rx, ry)
+                }
+                _ => (0.0, 0.0),
+            }
+        }
+        Side::Circular { start_angle } => todo!(),
     }
 }
 
 #[cfg(test)]
 mod reinf_tests {
+    use std::result;
+
+    use vputilslib::geometry2d::{Polygon, VpPoint};
+
     use crate::reinforcement::RebarData;
 
     use super::*;
@@ -191,11 +223,18 @@ mod reinf_tests {
         let mut equation_handler = EquationHandler::new();
         equation_handler.add_variable("L", 4000.0);
 
-        let rebar_coll1 : RebarCollection = RebarCollection {
-            reinf_data, offset_start, offset_end, concrete_cover: 30.0, side,
-            distribution: RebarDistribution::Even { 
-                diam: 16.0, count: 4, cc_left: "30".to_string(), cc_right: "30".to_string() },
-            
+        let rebar_coll1: RebarCollection = RebarCollection {
+            reinf_data,
+            offset_start,
+            offset_end,
+            concrete_cover: 30.0,
+            side,
+            distribution: RebarDistribution::Even {
+                diam: 16.0,
+                count: 4,
+                cc_left: "30".to_string(),
+                cc_right: "30".to_string(),
+            },
         };
         let result1 = rebar_coll1.get_single_rebars(&profile, &equation_handler);
         for rebar in &result1 {
@@ -220,19 +259,89 @@ mod reinf_tests {
         let mut equation_handler = EquationHandler::new();
         equation_handler.add_variable("L", 4000.0);
 
-        let rebar_coll1 : RebarCollection = RebarCollection {
-            reinf_data, offset_start, offset_end, concrete_cover: 30.0, side,
-            distribution: RebarDistribution::Even { 
-                diam: 16.0, count: 4, cc_left: "30".to_string(), cc_right: "30".to_string() },
-            
+        let rebar_coll: RebarCollection = RebarCollection {
+            reinf_data,
+            offset_start: offset_start.clone(),
+            offset_end: offset_end.clone(),
+            concrete_cover: 30.0,
+            side,
+            distribution: RebarDistribution::Even {
+                diam: 16.0,
+                count: 4,
+                cc_left: "30".to_string(),
+                cc_right: "30".to_string(),
+            },
         };
-        let result1 = rebar_coll1.get_single_rebars(&profile, &equation_handler);
-        for rebar in &result1 {
+        let result = rebar_coll.get_single_rebars(&profile, &equation_handler);
+        for rebar in &result {
             println!("X: {}, Y: {}", rebar.x, rebar.y);
         }
-        assert!(result1[0].x == 38.0);
-        assert!(result1[1].x == 106.0);
-        assert!(result1[2].x == 174.0);
-        assert!(result1[3].x == 242.0);
+        assert!(result[0].x == 38.0);
+        assert!(result[1].x == 106.0);
+        assert!(result[2].x == 174.0);
+        assert!(result[3].x == 242.0);
+
+        let rebar_coll: RebarCollection = RebarCollection {
+            reinf_data,
+            offset_start: offset_start.clone(),
+            offset_end: offset_end.clone(),
+            concrete_cover: 30.0,
+            side: Side::Polygon { index: 1 },
+            distribution: RebarDistribution::Even {
+                diam: 16.0,
+                count: 4,
+                cc_left: "30".to_string(),
+                cc_right: "30".to_string(),
+            },
+        };
+        let result = rebar_coll.get_single_rebars(&profile, &equation_handler);
+        for rebar in &result {
+            println!("X: {:.2}, Y: {:.2}", rebar.x, rebar.y);
+        }
+        assert!((result[0].y - 38.00).abs() < 0.01);
+        assert!((result[1].y - 172.67).abs() < 0.01);
+        assert!((result[2].y - 307.33).abs() < 0.01);
+        assert!((result[3].y - 442.00).abs() < 0.01);
+    }
+
+    #[test]
+    fn test_get_single_rebars_polygon_clockwise() {
+        let profile = Profile::new("name".to_string(), Polygon::new(vec![
+            VpPoint::new(280.0, 0.0),
+            VpPoint::new(0.0, 0.0),
+            VpPoint::new(0.0, 480.0),
+            VpPoint::new(280.0, 480.0),
+            VpPoint::new(280.0, 0.0),
+        ]));
+        let reinf_data = ReinforcementData::Rebar(RebarData {
+            yield_strength: 500.0,
+            elastic_modulus: 200e3,
+        });
+        let offset_start = "0".to_string();
+        let offset_end = "L".to_string();
+        let mut equation_handler = EquationHandler::new();
+        equation_handler.add_variable("L", 4000.0);
+
+        let rebar_coll: RebarCollection = RebarCollection {
+            reinf_data,
+            offset_start: offset_start.clone(),
+            offset_end: offset_end.clone(),
+            concrete_cover: 30.0,
+            side: Side::Polygon { index: 1 },
+            distribution: RebarDistribution::Even {
+                diam: 16.0,
+                count: 4,
+                cc_left: "30".to_string(),
+                cc_right: "30".to_string(),
+            },
+        };
+        let result = rebar_coll.get_single_rebars(&profile, &equation_handler);
+        for rebar in &result {
+            println!("X: {}, Y: {}", rebar.x, rebar.y);
+        }
+        assert!((result[0].y - 38.00).abs() < 0.01);
+        assert!((result[1].y - 172.67).abs() < 0.01);
+        assert!((result[2].y - 307.33).abs() < 0.01);
+        assert!((result[3].y - 442.00).abs() < 0.01);
     }
 }
